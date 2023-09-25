e42041c832c474751d8fbe21ddc3222d9ecc620b
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-25 20:18:41 +0300

    removed the fields in the Product class

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,56 +1,20 @@
-/*
-
-So when I call "new Product()"", I want to create a product with different values than the default values here.
-It would be nice if we could call "new Product()"" and just pass information to that product function,
-because we call it like a function,
-wouldn't it be nice if you could pass some arguments like a title, a price and so on here?
-
-=> Well, it turns out you can do that and for that, you go back to your class and you add a METHOD to that class.
-
-Now, we don't need a custom method,
-but now we need a special method, a method which Javascript automatically calls when we create a new instance of this class,
-and we create a new instance when we call "new Product()"" and then execute this class like a function,
-and this special method which Javascript executes for us is called the CONSTRUCTOR METHOD or the constructor function.
-
-*/
-
 class Product {
-  title = 'DEFAULT';
-  imageUrl;
-  description;
-  price;
+  // title = 'DEFAULT';
+  // imageUrl;
+  // description;
+  // price;
 
-  // someName() {} // a custom method is built like the method shorthand syntax
+  // => now we basically overwrite everything we set up here in the constructor and therefore you can actually remove that.
+  // This definition here is not required to be able to assign values in the constructor function.
+  // It will become important later again where we also work with fields which we don't initialize in the constructor.
 
   constructor(title, image, desc, price) {
     this.title = title;
-    // So "this.title" here refers to the property "title" which is added to product,
-    // "title" here refers to the "title" argument.
     this.imageUrl = image;
     this.description = desc;
     this.price = price;
-  }
+  } // all these things in the constructor function are PROPERTIES
 }
-// Now that will create a bunch of properties on the object which is created with this call below (new Product()),
-// and initialize the values of these properties with the values you passed to the constructor.
-
-/*
-
-The idea behind a constructor is that it can accept arguments like any normal method and there for example,
-we could accept a title, an image or imageUrl or however you want to name it, a description and a price.
-So we can accept any arguments, any parameters you want just like that,
-and in the curly braces and that's the interesting thing now, you can assign the values you're getting here for these parameters
-so you can assign the arguments you're getting, to your class fields,
-so to the properties of the object when it is instantiated then
-and you do this with the good old THIS keyword.
-=> "this" in here refers to your class (Product),
-or to be precise, since this class will be used to create an object, to the object that is created:
-So we can then for example say "this.title" and set it equal to the "title" argument.
-=> So "this.title" here refers to the property "title" which is added to product,
-so to the same property this field (title = 'DEFAULT';) would add,
-=> "title" here refers to the "title" argument.
-
-*/
 
 console.log(new Product());
 
@@ -61,7 +25,7 @@ const productList = {
       'https://img.freepik.com/psd-gratuit/oreiller-blanc-doux_176382-890.jpg?size=626&ext=jpg',
       'A soft pillow!',
       19.99
-    ), // now, we can pass in the values to it
+    ),
     new Product(
       'A Carpet',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
@@ -94,12 +58,3 @@ const productList = {
 };
 
 productList.render();
-
-/*
-
-So now we're using this class as a blueprint,
-and the huge advantage here is that we now have an easy, reusable way of creating objects,
-which are guaranteed to always look the same,
-it's impossible for us to omit properties or to mistype properties because it's all defined in here in this class definition.
-
-*/
